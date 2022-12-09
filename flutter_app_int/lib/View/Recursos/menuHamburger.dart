205f05284fac8fef4d76e471f42fbc6cfe717d84
kalebe rodrigues <kaleberodrigues52@gmail.com>
2022-12-08 22:35:07 -0300

    finalizado layout menu lateral

--- flutter_app_int/lib/View/Recursos/menuHamburger.dart
+++ flutter_app_int/lib/View/Recursos/menuHamburger.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,7 +8,6 @@ class MenuHamburger extends StatelessWidget {
   final String id = "55425";
   final String nome = "Tony";
   final String sobrenome = "Stark";
-  final String email = "tony@stark.com";
   final String foto = "img/stark.jpeg";
 
   Text fontePadrao(String texto, double tamanho, Color cor,
@@ -22,54 +23,63 @@ class MenuHamburger extends StatelessWidget {
     );
   }
 
+  Divider divisorPadrao() {
+    return Divider(height: 0, thickness: 1, indent: 5, endIndent: 5);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
           Container(
-              decoration: BoxDecoration(
-                  color: Colors.green.shade900,
-                  border: Border.all(color: Colors.green.shade900, width: 0)),
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              height: 30,
-              child: Row(children: [
-                fontePadrao("Olá Sr. ${sobrenome}", 18, Colors.white, 1, 3)
-              ])),
-          Container(
-              decoration: BoxDecoration(
-                  color: Colors.green.shade900,
-                  border: Border.all(color: Colors.green.shade900, width: 0)),
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
-              height: 110,
-              child: Row(children: [
+            alignment: Alignment.bottomLeft,
+            height: 140,
+            padding: EdgeInsets.all(10),
+            color: Color.fromRGBO(0, 194, 203, 1),
+            child: Row(
+              children: [
                 Column(
                   children: [
-                    SizedBox(height: 85),
-                    fontePadrao("ID: ${id}", 12, Colors.grey, 1, 3)
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      height: 80,
+                      width: 150,
+                      child: fontePadrao(
+                          "Olá, Sr. ${sobrenome}", 18, Colors.white, 1, 3),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomLeft,
+                      height: 40,
+                      width: 150,
+                      child: fontePadrao("ID: ${id}", 12, Colors.white, 1, 3),
+                    )
                   ],
                 ),
                 SizedBox(
-                  width: 109,
+                  width: 20,
                 ),
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
+                        borderRadius: BorderRadius.circular(60),
                         child: Image.asset(
                           "img/stark.jpeg",
-                          height: 100,
-                          width: 100,
+                          height: 110,
+                          width: 110,
                         ))
                   ],
-                ),
-              ])),
+                )
+              ],
+            ),
+          ),
           ListTile(
             title:
                 fontePadrao("Umidade do solo", 15, Colors.grey.shade800, 0, 3),
             leading: Icon(
               Icons.water_drop_sharp,
-              color: Colors.blue.shade700,
+              color: Color.fromRGBO(9, 117, 155, 1),
               size: 22,
             ),
             trailing: Icon(
@@ -79,11 +89,74 @@ class MenuHamburger extends StatelessWidget {
             ),
             onTap: () {},
           ),
+          divisorPadrao(),
           ListTile(
             title: fontePadrao("Umidade do ar", 15, Colors.grey.shade800, 0, 3),
             leading: Icon(
               Icons.water_drop_sharp,
-              color: Colors.blue.shade700,
+              color: Color.fromRGBO(9, 117, 155, 1),
+              size: 22,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.grey.shade800,
+              size: 14,
+            ),
+            onTap: () {},
+          ),
+          divisorPadrao(),
+          ListTile(
+            title: fontePadrao(
+                "Temperatura do solo", 15, Colors.grey.shade800, 0, 3),
+            leading: Icon(
+              Icons.thermostat_sharp,
+              color: Color.fromRGBO(77, 171, 199, 1),
+              size: 22,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.grey.shade800,
+              size: 14,
+            ),
+            onTap: () {},
+          ),
+          divisorPadrao(),
+          ListTile(
+            title: fontePadrao(
+                "Temperatura ambiente", 15, Colors.grey.shade800, 0, 3),
+            leading: Icon(
+              Icons.thermostat_sharp,
+              color: Color.fromRGBO(77, 171, 199, 1),
+              size: 22,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.grey.shade800,
+              size: 14,
+            ),
+            onTap: () {},
+          ),
+          divisorPadrao(),
+          ListTile(
+            title: fontePadrao("Luminosidade", 15, Colors.grey.shade800, 0, 3),
+            leading: Icon(
+              Icons.wb_sunny_sharp,
+              color: Colors.amber.shade400,
+              size: 22,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.grey.shade800,
+              size: 14,
+            ),
+            onTap: () {},
+          ),
+          divisorPadrao(),
+          ListTile(
+            title: fontePadrao("Configurações", 15, Colors.grey.shade800, 0, 3),
+            leading: Icon(
+              Icons.settings,
+              color: Colors.grey.shade800,
               size: 22,
             ),
             trailing: Icon(
--- flutter_app_int/lib/View/home.dart
+++ flutter_app_int/lib/View/home.dart
@@ -14,7 +14,27 @@ class HomeState extends State<Home> {
     return Scaffold(
       appBar: BarraSuperior(),
       drawer: MenuHamburger(),
-      body: Text(" "),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Text("Solo"),
+            iconSize: 30,
+          ),
+          VerticalDivider(
+            width: 1,
+            thickness: 1,
+            indent: 10,
+            endIndent: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Text("Ar"),
+            iconSize: 30,
+          ),
+        ],
+      ),
     );
   }
 }
